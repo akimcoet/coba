2ff6c512bb8f4c01be8f2105f03d4692007da83a
akimcoet <68885079+akimcoet@users.noreply.github.com>
2020-08-04 11:24:47 +0700

    jl

--- function1.php
+++ function1.php
@@ -56,7 +56,7 @@ $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -7.869484,110.437979;
+$header[] = "X-Location: -7.869484,110.437979";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
